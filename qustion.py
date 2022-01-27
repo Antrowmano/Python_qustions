a9ca8bbb63723a34a043a91d3ac5760edb8e7696
antrow <antrowmano543@gmail.com>
2022-01-27 11:49:38 +0530

    first commit

---
+++ qustion.py
@@ -0,0 +1,19 @@
+# 1.Print even numbers between 1 and 100
+
+for i in range(101):
+    if (i % 2 ==0):
+        print(f"{i}")
+
+
+# 2.Write a function which will return set of given list
+#	Input : [2,3,5] 
+#	Output : (4,9,25) 
+
+number = [2,3,5] 
+for numbers in number:
+    print(numbers ** 2)
+
+
+
+
+
